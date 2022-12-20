aed93f21a0bb36ee6fec114ee995279f0da2a35c
Haena Lee <haenalee0721@gmail.com>
2022-12-21 02:47:53 +0900

    [12/21] 해결완

--- BJ_10828.cpp
+++ BJ_10828.cpp
@@ -6,54 +6,36 @@ using namespace std;
 
 int main() {
 
-	int n=0,b=0,c=0;
+	int n,b;
 	string a;
 	stack<int> s;
 
-	cin >> n;
-
 	int* result = new int[n];
 
-	
+	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> a;
 		if (a == "push") {
 			cin >> b;
 			s.push(b);
 		}else if(a == "top") {
-			if (s.empty()) { result[c] = -1; c++; }
-			else {
-				result[c] = s.top();
-				c++;
-			}
+			if (s.empty()) cout << -1<<"\n";
+			else cout<<s.top()<<"\n";
 		}
 		else if (a == "size") {
-			result[c] = s.size();
-			c++;
+			cout << s.size() << "\n";
 		}
 		else if (a == "empty") {
-			if (s.empty()) {
-				result[c] = 1; c++;
-			}
-			else {
-				result[c] = 0; c++;
-			}
+			if (s.empty()) cout << 1 << "\n";
+			else cout << 0 << "\n";
 		}
 		else {
-			if (s.empty()) {
-				result[c] = -1; c++;
-			}
+			if (s.empty()) cout << -1 << "\n";
 			else {
-				result[c] = s.top(); c++;
+				cout << s.top() << "\n";
 				s.pop();
 			}
 		}
 		
 	}
-
-	for (int i = 0; i < c-1; i++) {
-		cout << result[i] << "\n";
-	}cout << result[c - 1];
-
-	delete[] result;
 }
