bbd087408f0c7f95bbd7e235514419a6ca60ce8c
Haena Lee <haenalee0721@gmail.com>
2023-01-20 03:08:06 +0900

    [ 1/20 ]  완벽한 해결

--- BJ_2178.cpp
+++ BJ_2178.cpp
@@ -7,46 +7,48 @@
 using namespace std;
 
 int check[100][100];
+int n, m;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+string a[100];
 
 int main() {
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, m,num=0;
-	int dx[4] = {0,0,1,-1};
-	int dy[4] = {1,-1,0,0};
-	int a[100][100];
-	string line;
-
-	queue<pair<int, int>> Q;
-
 	cin >> n >> m;
 
-	for (int i = 0; i < n; i++) {
-		getline(cin, line);
-		int j=0;
-		for (auto c : line) {
-			a[i][j++] = c;
-		}
-	}
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
 
-	while(!Q.empty()) {
-		pair<int, int> C = Q.front();
+	queue<pair<int, int>> Q;
+	Q.push({ 0,0 });
+	check[0][0] = 1;
+
+
+	while (!Q.empty()) {
+		auto C = Q.front();
+
 		Q.pop();
+	
+
 		for (int i = 0; i < 4; i++) {
 			int nx = C.first + dx[i];
 			int ny = C.second + dy[i];
 
-			if (nx<0 || nx>n || ny<0 || ny>m) continue;
-			if (a[nx][ny] == 0 || check[nx][ny] == 1) continue;
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				continue;
+			}
+			if (a[nx][ny] == '0' || check[nx][ny] >= 1) {
+				continue;
+			}
 
-			check[nx][ny] =1;
+			check[nx][ny] = check[C.first][C.second] + 1;
 			Q.push({ nx,ny });
-			num++;
-		}
 
+		}
 	}
 
-	cout << num;
+	cout << check[n - 1][m - 1];
 }
